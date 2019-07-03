e18a9d0e80f05ff2dc42d237169d17c23e91081e
Tom Pearson <tom.g.pearson@gmail.com>
2019-07-03 17:12:31 +0100

    add helmet

--- index.js
+++ index.js
@@ -2,20 +2,18 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const app = express();
-const port = process.env.PORT || 3000;
+const helmet = require('helmet')
 const addRoutes = require('./routes');
 const addFilters = require('./filters');
 
+app.use(helmet());
 
-const env = nunjucks.configure('views', {
-  autoescape: true,
-  express: app
-});
+const env = nunjucks.configure('views', { autoescape: true, express: app });
 
 addFilters(env);
-
 addRoutes(app);
 
+const port = process.env.PORT || 3000;
 app.listen(port, function(){
   console.log(`ATF Premiership Rank is running on ${port}`);
 });
